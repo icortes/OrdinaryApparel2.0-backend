54609a91ade659d1e8acf00dfb0f927cc23bb17c
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2024-01-18 14:59:50 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,35 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  headers: {
-    'Access-Control-Allow-Origin': ['http://localhost:5502'],
+  async headers() {
+    return [
+      {
+        //route this applies to
+        source: '/api/(.*)',
+        //headers
+        headers: [
+          //allow for specific domains to have access or * for all
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          //allows for specific methods accepted
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          //allows for specific headers accepted (These are a few standard ones)
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
   },
-
-  // async headers() {
-  //   return [
-  //     {
-  //       //route this applies to
-  //       source: '/api/(.*)',
-  //       //headers
-  //       headers: [
-  //         //allow for specific domains to have access or * for all
-  //         {
-  //           key: 'Access-Control-Allow-Origin',
-  //           value: '*',
-  //         },
-  //         //allows for specific methods accepted
-  //         {
-  //           key: 'Access-Control-Allow-Methods',
-  //           value: 'GET, POST, PUT, DELETE, OPTIONS',
-  //         },
-  //         //allows for specific headers accepted (These are a few standard ones)
-  //         {
-  //           key: 'Access-Control-Allow-Headers',
-  //           value: 'Content-Type, Authorization',
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
 };
 
 module.exports = nextConfig;
