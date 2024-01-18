1d752d22f0460d7b2bf80863ba815e228438ef70
Isaac Cortes Hernandez <isaaccortes014@gmail.com>
2024-01-18 14:35:01 -0800

    removing cors for local development

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        //route this applies to
+        source: '/api/(.*)',
+        //headers
+        headers: [
+          //allow for specific domains to have access or * for all
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          //allows for specific methods accepted
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          //allows for specific headers accepted (These are a few standard ones)
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
